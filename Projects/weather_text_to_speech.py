061c7002583d0d21c135d1bbc398f105cde18c0f
Ritesh Kumar <93812163+Ritesh-zt0@users.noreply.github.com>
2023-05-11 22:45:37 +0530

    Added Weather Text-to-Speech

---
+++ Projects/weather_text_to_speech.py
@@ -0,0 +1,23 @@
+import os
+import requests
+import json
+import win32com.client
+
+speaker = win32com.client.Dispatch("SAPI.SpVoice")
+
+city = input("Enter the name of the city: \n")
+
+url = f"https://api.weatherapi.com/v1/current.json?key=a94de503031344f886f153530231105&q={city}"
+
+r = requests.get(url)
+# print(r.text)
+wdic = json.loads(r.text)
+w = wdic["current"]["temp_c"]
+
+while 1:
+    speaker.Speak(f" The current weather in {city} is {w} degrees")
+
+# stop the py file for the output voice to stop
+
+
+
